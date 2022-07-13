c2ab96af610b0785d7754d6b80749189dff8c784
Anand Sahu <71924059+Anandsahujnu11@users.noreply.github.com>
2022-07-13 11:57:57 +0530

    Added initial tooltip description
    for "upload geometry" and "select geometry"

---
+++ litesoph/gui/tooltipdoc.py
@@ -0,0 +1,6 @@
+from tkinter import font
+import tkinter as tk
+
+title = "Hello world"
+upload_geometry ='Please upload the geometry file i.e xyz coordinate file using "SELECT" button'
+select_geometry ='Please upload your xyz coordinate file here'
